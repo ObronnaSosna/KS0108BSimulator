662d166931efa5d5b21ade0ffc2a42bb01393c37
ObronnaSosna <kazokozak@gmail.com>
2024-05-14 23:34:21 +0200

    dout hotfix

--- build_cross.sh
+++ build_cross.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wine /home/$(whoami)/.wine/drive_c/users/$(whoami)/AppData/Local/Programs/Python/Python312/Scripts/pyinstaller.exe -F --noconsole main.py
+wine /home/$(whoami)/.wine/drive_c/users/$(whoami)/AppData/Local/Programs/Python/Python312/Scripts/pyinstaller.exe -F --noconsole main.py --additional-hooks-dir 'C:\\users\\stefan\\AppData\\Local\\Programs\\Python\\Python312\\Lib\\site-packages\\serial\\__init__.py'
 mv dist/main.exe dist/KS0108BSimulator_windows.exe
 
